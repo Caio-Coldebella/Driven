9a90d8fc476fc7ad8fee5b2c9fab1126f1665f4d
Caio-Coldebella <c232621@dac.unicamp.br>
2022-06-25 22:25:22 -0300

    bonus de mensagem privada e versao final

--- projeto5-batepapouol/scripts/scripts.js
+++ projeto5-batepapouol/scripts/scripts.js
@@ -1,8 +1,65 @@
 let user = "";
+let listofusers = [];
+let visibility = "message";
+let receivers = "Todos";
+function selectreceiver(element){
+    let listofelements = document.querySelectorAll(".contacts .checkmark");
+    for(let i = 0; i< listofelements.length; i++){
+        listofelements[i].classList.add("hidden");
+    }
+    element.querySelector(".hidden").classList.remove("hidden");
+    receivers = element.querySelector(".txtparticipant").innerHTML;
+}
+function selectvisibility(element){
+    let listofvisb = document.querySelectorAll(".visibilities .checkmark");
+    for(let i = 0; i< listofvisb.length; i++){
+        listofvisb[i].classList.add("hidden");
+    }
+    element.querySelector(".hidden").classList.remove("hidden");
+    let txt = element.querySelector(".txtvisibility").innerHTML;
+    if(txt === "Público"){
+        visibility = "message";
+    }else if(txt === "Reservadamente"){
+        visibility = "private_message";
+    }
+}
+function closesidebar(){
+    document.querySelector(".blackbackground").classList.add("hidden");
+    document.querySelector(".sidebar").classList.add("hidden");
+}
+function opensidebar(){
+    document.querySelector(".blackbackground").classList.remove("hidden");
+    document.querySelector(".sidebar").classList.remove("hidden");
+}
 
+function addparticipants(participants){
+    listofusers = participants.data;
+    let list = document.querySelector(".contacts");
+    list.innerHTML = `<div onclick="selectreceiver(this)">
+    <div>
+        <ion-icon name="people"></ion-icon>
+        <p class=txtparticipant>Todos</p>
+    </div>
+    <ion-icon name="checkmark" class="checkmark hidden"></ion-icon>
+</div>`;
+    for(let i=0; i<listofusers.length; i++){
+        let element = `<div data-identifier="participant" onclick="selectreceiver(this)">
+        <div>
+            <ion-icon name="person-circle"></ion-icon>
+            <p class="txtparticipant">${listofusers[i].name}</p>
+        </div>
+        <ion-icon name="checkmark" class="checkmark hidden"></ion-icon>
+    </div>`;
+        list.innerHTML += element;
+    }
 
+}
+function getparticipants(){
+    let promisse = axios.get("https://mock-api.driven.com.br/api/v6/uol/participants");
+    promisse.then(addparticipants);
+    promisse.catch((error)=>console.log(error.response.status));
+}
 let sendsucess = (sucess) => {
-    console.log("pao")
     reloadmessages();}
 function sendfail(error){
     console.log(error.response.status)
@@ -11,9 +68,9 @@ function sendmessage(){
     let message = document.querySelector(".bottombar input");
     let object = {
         from: user,
-        to: "Todos",
+        to: receivers,
         text: message.value,
-        type: "message" // ou "private_message" para o bônus
+        type: visibility
     }
     message.value = "";
     let promisse = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages",object);
@@ -37,7 +94,7 @@ function showmessages(messages){
             <p><span class="time">${message.time} </span><em>${message.from} </em>${message.text}</p>
             </div>`;
         }else if(message.type === "private_message"){
-            if(message.to !== user){
+            if(message.to !== user && message.from !== user && message.to !== "Todos"){
                 continue;
             }
             msg = `<div class="generalmessage private_message">
@@ -64,6 +121,9 @@ function sucessfullconnection(sucess){
     let statuscode = sucess.status;
     setInterval(()=>{axios.post("https://mock-api.driven.com.br/api/v6/uol/status", {name: user})},5000);
     setInterval(()=>{reloadmessages();},3000);
+    setInterval(()=>{getparticipants();},10000);
+    reloadmessages();
+    getparticipants();
 }
 function failedconnection(fail){
     let statuscode = fail.response.status;
